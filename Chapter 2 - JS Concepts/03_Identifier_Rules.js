eb83e9c76e578768371bde74d35832612a19411d
Madhuripanda <madhuripanda210@gmail.com>
2026-07-15 21:09:13 +0530

    Chapter 2 commit

---
+++ Chapter 2 - JS Concepts/03_Identifier_Rules.js
@@ -0,0 +1,33 @@
+var a = 10;
+console.log(a);
+
+var $ = 10;
+var _a = 23;
+var pp = 34;
+
+var ab123 = 23;
+//var 45 = 34;
+var _ = 10;
+
+var Name = "Madhu";
+var name = "Madhuri"
+
+//var parmod dutta = "hello";
+var madhuri_panda = "hello";
+var madhuri$panda = "hello";
+var Madhu132 = 'hello';
+
+//1.camelCase--standard for JS variables and functions
+let userName = "camelCase";
+
+//2. PascalCase --standard for JS classes and constructors
+let UserProfile = "PascalCase";
+
+//3. snake_case(underscore seperated)
+let user_name = "snake_case";
+
+//4. SCREAMING_SNAKE_CASE (constants)
+const MAX_SIZE = 100;
+
+///5. Hungarian Notation --prefix with type--olserstyle
+let strName ="string prefix";
