4dda06f67a9be0af3fee60ea7784c9acdd9803a9
Madhuripanda <madhuripanda210@gmail.com>
2026-07-27 23:30:42 +0530

    Practice topics

---
+++ 05_chapter_Operator/33_Adv_increment.js
@@ -0,0 +1,16 @@
+// let a = 10;
+// console.log(++a + a);
+// console.log(a);
+
+
+// let i=1;
+// let result = i++ + ++i;
+// console.log(result, i);
+
+// let b =10;
+// console.log(++b + ++b);
+// console.log(b);
+
+let i =1;
+let r = i++>1 ? i++ : ++i;
+console.log(r,i);
---
+++ 07_Chapter_switch/39_Switch.js
@@ -0,0 +1,30 @@
+let day =2;
+switch(day){
+    case 1:
+        console.log('Mon');
+        break;
+        case 2:
+        console.log('Tues');
+        let a =10;
+        let b= 20;
+        console.log(a+b);
+        break;
+        case 3:
+        console.log('Wed');
+        break;
+        case 4:
+        console.log('Thrus');
+        break;
+        case 5:
+        console.log('Fri');
+        break;
+        case 6:
+        console.log('Sat');
+        break;
+        case 7:
+        console.log('Sun');
+        break;
+        default:
+            console.log('Noidea which day');
+
+}
